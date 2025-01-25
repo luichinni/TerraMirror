048f424f8c325ed0b5b9be86e44dea04dccb45fb
Luciano <lumacias11702@gmail.com>
2025-01-24 23:27:50 -0300

    Initial commit

---
+++ src/main/java/dev/lordvi/terramirror/TerraMirror.java
@@ -0,0 +1,9 @@
+package dev.lordvi.terramirror;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TerraMirror.MODID)
+public class TerraMirror {
+    public static final String MODID = "terra_mirror";
+
+}
